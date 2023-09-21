27add4ab787acded155cb0043cc992b093a65894
Fernando Martínez <45270620+ferdmartin@users.noreply.github.com>
2023-09-20 21:03:53 -0400

    Update TrainingAndAnalysis.py

--- src/TrainingAndAnalysis.py
+++ src/TrainingAndAnalysis.py
@@ -22,6 +22,18 @@ import re
 
 nlp = spacy.load('en_core_web_sm')
 
+# Examples
+test_text_AI = """During my undergraduate studies, I gained a solid foundation in programming languages such as Python and R, as well as experience working with SQL databases. I also had the opportunity to apply these skills in a real-world setting during an internship as a data analyst at a healthcare company. In this role, I was responsible for collecting, cleaning, and analyzing large datasets to provide insights into patient outcomes and healthcare costs.
+Through the Master's in Data Science program at Fordham University, I aim to further develop my expertise in data science and analytics, with a focus on machine learning and predictive modeling. I am particularly interested in courses that cover topics such as deep learning, natural language processing, and data visualization. I am confident that this program will provide me with the skills and knowledge necessary to make valuable contributions to the field of data science.
+Furthermore, I am impressed with the collaborative and interdisciplinary nature of the program, and I am excited about the opportunity to work with fellow students and faculty members from diverse backgrounds and fields. I am also attracted to the program's emphasis on practical, hands-on learning, which I believe will prepare me well for a career in data science.
+Thank you for considering my application. I am excited about the prospect of joining the Fordham University community and contributing to the vibrant academic and research environment of the Master's in Data Science program.
+Sincerely,"""
+
+test_text_mixed_Human_AI = """Though I was barely exposed to programming while doing my B.A. in Economics, my eagerness to face new challenges led me to learn many computer languages like R and SQL. Ultimately, I decided to pursue a master's in Data Science at Fordham University, where I am fortunate to hold a Graduate Assistant position in Fordham's Computer and Information Sciences (CIS) department. Some projects have exposed me to the undertakings of high-level research and the massive amount of objectives and tasks necessary to transform a research proposal into a ranked article publication. But, most importantly, I have comprehended the role researchers play in advancing the computer science field and the gratification that yields, having contributed to it. As a result, I am driven to pursue a Ph.D. in Computer Science at NYU Tandon School of Engineering to broaden my knowledge and become an established researcher.
+In addition to my academic pursuits, I have also gained valuable experience through internships and extracurricular activities. During my undergraduate years, I interned at a non-profit organization where I assisted in developing a database management system. This experience allowed me to apply my programming skills in a real-world setting and reinforced my interest in pursuing a career in technology.
+Furthermore, I have been an active member of the Computer Science Club, where I have participated in various coding competitions and hackathons. These experiences have not only honed my technical skills but also taught me the importance of teamwork and collaboration in solving complex problems. I have also volunteered as a tutor, helping students with programming and data analysis. This experience has allowed me to share my knowledge with others and has reinforced my desire to pursue a career in academia.
+As I embark on the next stage of my academic journey, I am eager to continue building on my experiences and knowledge. I am confident that pursuing a Ph.D. in Computer Science at NYU Tandon School of Engineering will provide me with the tools and resources necessary to achieve my academic and professional goals. I look forward to contributing to the vibrant research community at NYU and making meaningful contributions to the field of Computer Science."""
+
 def text_cleaning(text):
     """
     This function takes a string as input and returns a formatted version of the string. 
@@ -286,4 +298,118 @@ Custom_HF_Model = MyHFModel(config)
 
 Custom_HF_Model.save_pretrained("HF_DistilBertBasedModelAppDocs2")
 
+##########
 # Bert
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Encode the text data
+train_encodings = tokenizer(train_sentences, truncation=True, padding=True)
+val_encodings = tokenizer(test_sentences, truncation=True, padding=True)
+
+# Include additional features
+columns = X_train.drop(["Text"],axis=1).columns.to_list()
+
+for column in columns:
+    train_encodings[column] = X_train[column].to_list()
+    val_encodings[column] = X_test[column].to_list()
+
+train_dataset = TextDataset(train_encodings, y_train.values)
+val_dataset = TextDataset(val_encodings, y_test.values)
+
+batch_size = 8
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+class TransformerBasedModelBert(nn.Module):
+    def __init__(self):
+        super(TransformerBasedModelBert, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.dropout = nn.Dropout(0.55)
+        self.fc = nn.Linear(768, 2)
+
+    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+        input_shape = input_ids.size()
+        if attention_mask is None:
+            attention_mask = torch.ones(input_shape, device=device)
+            
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.fc(pooled_output)
+        return logits
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = TransformerBasedModelBert().to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+loss_fn = nn.CrossEntropyLoss()
+early_stopping = EarlyStopping()
+
+num_epochs = 5
+for epoch in range(num_epochs):
+    model.train()
+    train_loss = 0
+    train_total = 0
+    for batch in train_loader:
+      input_ids = batch['input_ids'].to(device)
+      attention_mask = batch['attention_mask'].to(device)
+      labels = batch['labels'].to(device)
+      logits = model(input_ids=input_ids, attention_mask=attention_mask)
+      loss = loss_fn(logits, labels)
+
+      train_loss += loss
+      train_total += labels.size(0)
+
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+    model.eval()
+    with torch.inference_mode():
+        correct = 0
+        test_loss = 0
+        total = 0
+        for batch in val_loader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            logits = model(input_ids=input_ids, attention_mask=attention_mask)
+            test_loss += loss_fn(logits, labels)
+            _, predicted = torch.max(logits, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        
+    early_stop = early_stopping(test_loss/total, model)
+    
+    if early_stop:
+        print("Early stopping")
+        break
+
+    print(f'Epoch {epoch+1} out of {num_epochs} | Train Loss: {train_loss/train_total:.6f} | Test Accuracy: {correct/total:.6f}')
+
+### best weights
+model.load_state_dict(torch.load('checkpoint.pt'))
+
+class MyConfig(PretrainedConfig):
+    model_type = "bert"
+    def __init__(self, final_dropout=0.55, **kwargs):
+        super().__init__(**kwargs)
+        self.final_dropout = final_dropout
+
+class MyHFModel_BertBased(PreTrainedModel):
+    config_class = MyConfig
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.model = model
+    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+        input_shape = input_ids.size()
+        if attention_mask is None:
+            attention_mask = torch.ones(input_shape, device=device)
+        return self.model(input_ids=input_ids, attention_mask=attention_mask)
+
+config = MyConfig(0.55)
+Custom_HF_Model = MyHFModel_BertBased(config)
+
+Custom_HF_Model.save_pretrained("HF_BertBasedModelAppDocs2")
